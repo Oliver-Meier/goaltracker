ef99d7c962632f542cf2d4a3f32d18a83795db5f
Oliver <oliver.meier@sap.com>
2024-02-22 20:14:34 +0100

    adopt schemas and entities to tracker model

--- srv/entities.ts
+++ srv/entities.ts
@@ -1,48 +1,17 @@
-interface Room {
-    ID: number,
-    name: string,
-    seats: number
-    lectures: Lecture[]
+interface Hobby {
+    ID: string,
+    name: string
 }
 
-interface Course {
+interface Record {
     ID: string,
     name: string,
-    descr: string,
-    ects: number,
-    lectures: Lecture[]
-}
-
-interface Professor {
-    ID: number,
-    firstname: string,
-    lastname: string,
-    title: string,
-    lectures: Lecture[]
-}
-
-interface Lecture {
-    ID: number,
+    hobby: string,
     starttime: Date,
     endtime: Date,
-    course: Course,
-    course_ID: string,
-    prof: Professor,
-    prof_ID: number,
-    room: Room,
-    room_ID: number,
-    allday: boolean
-}
-
-interface AlldayEvent {
-    reqSeats: number,
-    date: Date
 }
 
 export {
-    Room,
-    Course,
-    Professor,
-    Lecture,
-    AlldayEvent
+    Hobby,
+    Record
 }
